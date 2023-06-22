6fdcb73bb40cd20372fb6be48cf5a2316119c9bd
Chris <chris.kamau@student.moringaschool.com>
2023-06-22 13:45:07 +0300

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+    validate :sufficiently_clickbait_y_title
+  
+    private
+  
+    def sufficiently_clickbait_y_title
+      unless title.present? &&
+             (title.include?("Won't Believe") ||
+              title.include?("Secret") ||
+              title.match?(/\bTop \d+\b/) ||
+              title.include?("Guess"))
+        errors.add(:title, "must be sufficiently clickbait-y")
+      end
+    end
+  end
+  
